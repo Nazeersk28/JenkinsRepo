3a91138e448aa09eba3db4acd649f214c7f97333
iomegak12 <jd.ramkumar@gmail.com>
2019-03-27 19:30:24 +0530

    changes

--- jenkins/scripts/run-container.sh
+++ jenkins/scripts/run-container.sh
@@ -2,6 +2,8 @@
 
 set -x 
 
-docker run -d -t -p 9090:8080 -e ASPNETCORE_URLS=http://*:8080 -e CRMSystemDBConnectionString=ZGF0YSBzb3VyY2U9aW9tZWdhc3Fsc2VydmVydjIuZGF0YWJhc2Uud2luZG93cy5uZXQ7dXNlciBpZD1pb21lZ2FhZG1pbjtwYXNzd29yZD1QcmVzdGlnZTEyMztpbml0aWFsIGNhdGFsb2c9aW9tZWdhc3FsZGF0YWJhc2V2Mjs= --name restservices iomega/cgiprofessionalnetcoreservicesv2
+echo "Running the Container with the name ... ${CONTAINER_NAME}"
+
+docker run -d -t -p 9090:8080 -e ASPNETCORE_URLS=http://*:8080 -e CRMSystemDBConnectionString=ZGF0YSBzb3VyY2U9aW9tZWdhc3Fsc2VydmVydjIuZGF0YWJhc2Uud2luZG93cy5uZXQ7dXNlciBpZD1pb21lZ2FhZG1pbjtwYXNzd29yZD1QcmVzdGlnZTEyMztpbml0aWFsIGNhdGFsb2c9aW9tZWdhc3FsZGF0YWJhc2V2Mjs= --name ${CONTAINER_NAME} iomega/cgiprofessionalnetcoreservicesv2
 
 set +x
