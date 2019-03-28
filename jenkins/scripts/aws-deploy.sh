d98e52b24d4666e624f58de34928d9d2346c8664
iomegak12 <jd.ramkumar@gmail.com>
2019-03-29 02:37:16 +0530

    Update aws-deploy.sh

--- jenkins/scripts/aws-deploy.sh
+++ jenkins/scripts/aws-deploy.sh
@@ -1,8 +1,8 @@
-aws ecs create-cluster --cluster-name fargate-cluster
+aws ecs create-cluster --cluster-name ${CLUSTER_NAME}
 
 aws ecs register-task-definition --cli-input-json file://fargate-task.json
 
 aws ecs list-task-definitions
 
-aws ecs create-service --cluster fargate-cluster --service-name fargate-service --task-definition cgiprofessionalnetcoreservices --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-0008f92ed5887c067],securityGroups=[sg-08fb93c325f5ae436],assignPublicIp=ENABLED}"
+aws ecs create-service --cluster ${CLUSTER_NAME} --service-name fargate-service --task-definition cgiprofessionalnetcoreservices --desired-count 1 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-0008f92ed5887c067],securityGroups=[sg-08fb93c325f5ae436],assignPublicIp=ENABLED}"
 
